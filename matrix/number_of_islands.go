7f65eccb66ede0062e96911f287a55dd27df96ab
Vlad <vlad@Vlads-MacBook-Pro.local>
2020-08-06 17:41:35 +0300

    Added ValidSudoku

--- matrix/number_of_islands.go
+++ matrix/number_of_islands.go
@@ -10,22 +10,22 @@ import (
 // Given a 2-d grid map of '1's (land) and '0's (water), count the number of islands.
 // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
 // You may assume all four edges of the grid are all surrounded by water.
-func NumberOfIslands(arr [][]rune) {
-	if len(arr[0]) == 0 || len(arr) == 0 {
+func NumberOfIslands(mx [][]rune) {
+	if len(mx[0]) == 0 || len(mx) == 0 {
 		fmt.Printf("Count of islands: 0\n")
 		return
 	}
 
-	n := len(arr)
-	m := len(arr[0])
+	n := len(mx)
+	m := len(mx[0])
 	count := 0
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
-			if arr[i][j] == '1' {
+			if mx[i][j] == '1' {
 				count++
-				merge(arr, i, j)
+				merge(mx, i, j)
 				fmt.Printf("Matrix after merge phase:\n")
-				utils.PrintMatrix(arr)
+				utils.PrintMatrix(mx)
 			}
 		}
 	}
@@ -33,17 +33,17 @@ func NumberOfIslands(arr [][]rune) {
 	fmt.Printf("Count of islands: %v\n", count)
 }
 
-func merge(arr [][]rune, i int, j int) {
-	n := len(arr)
-	m := len(arr[0])
+func merge(mx [][]rune, i int, j int) {
+	n := len(mx)
+	m := len(mx[0])
 
-	if i < 0 || i >= n || j < 0 || j >= m || arr[i][j] != '1' {
+	if i < 0 || i >= n || j < 0 || j >= m || mx[i][j] != '1' {
 		return
 	}
 
-	arr[i][j] = 'X'
-	merge(arr, i-1, j)
-	merge(arr, i+1, j)
-	merge(arr, i, j-1)
-	merge(arr, i, j+1)
+	mx[i][j] = 'X'
+	merge(mx, i-1, j)
+	merge(mx, i+1, j)
+	merge(mx, i, j-1)
+	merge(mx, i, j+1)
 }
--- matrix/valid_sudoku.go
+++ matrix/valid_sudoku.go
@@ -2,10 +2,65 @@ package matrix
 
 import (
 	"fmt"
+
+	"github.com/morzhanov/algorithm-problem-solutions/utils"
 )
 
 // ValidSudoku test
-// TODO: add description
-func ValidSudoku(str string) {
-	fmt.Printf("Not yet implemented")
+// Determine if a Sudoku is valid.
+// The Sudoku board could be partially filled, where empty cells are filled with the character '.'
+func ValidSudoku(mx [][]rune) {
+	if len(mx) != 9 || len(mx[0]) != 9 {
+		fmt.Printf("Matrix is not valid sudoku board\n")
+		utils.PrintMatrix(mx)
+	}
+
+	for i := 0; i < 9; i++ {
+		arr := make([]rune, 9)
+		for j := 0; j < 9; j++ {
+			el := mx[i][j]
+			if el != '.' {
+				if utils.Pos(arr, el) != -1 {
+					fmt.Printf("Matrix is not valid sudoku board (ROW check phase)\n")
+					fmt.Printf("ROW = %v, COLUMN = %v, VALUE = %v\n", i, j, el)
+					return
+				}
+				arr = append(arr, el)
+			}
+		}
+	}
+
+	for i := 0; i < 9; i++ {
+		arr := make([]rune, 9)
+		for j := 0; j < 9; j++ {
+			el := mx[j][i]
+			if el != '.' {
+				if utils.Pos(arr, el) != -1 {
+					fmt.Printf("Matrix is not valid sudoku board (COLUMN check phase)\n")
+					fmt.Printf("ROW = %v, COLUMN = %v, VALUE = %v\n", i, j, el)
+					return
+				}
+				arr = append(arr, el)
+			}
+		}
+	}
+
+	for block := 0; block < 9; block++ {
+		arr := make([]rune, 9)
+		for i := block / 3 * 3; i < block/3*3+3; i++ {
+			for j := block % 3 * 3; j < block%3*3+3; j++ {
+				el := mx[i][j]
+				if el != '.' {
+					if utils.Pos(arr, el) != -1 {
+						fmt.Printf("Matrix is not valid sudoku board (BLOCK check phase)\n")
+						fmt.Printf("BLOCK = %v, ROW = %v, COLUMN = %v, VALUE = %v\n", block, i, j, el)
+						return
+					}
+					arr = append(arr, el)
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Matrix is a valid sudoku board\n")
 }
