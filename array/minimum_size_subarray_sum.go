cc7aa2f44acfbfecb7d89b4171d16b05c19be07b
Vlad <vlad@Vlads-MacBook-Pro.local>
2020-08-11 20:21:40 +0300

    added base MinimumSizeSubarraySum

---
+++ array/minimum_size_subarray_sum.go
@@ -0,0 +1,18 @@
+package array
+
+// MinimumSizeSubarraySum test
+// Given an array of n positive integers and a positive integer s,
+// find the minimal length of a subarray of which the sum ≥ s.
+// If there isn't one, return 0 instead.
+// For example, given the array [2,3,1,2,4,3] and s = 7,
+// the subarray [4,3] has the minimal length of 2 under the problem constraint.
+func MinimumSizeSubarraySum(arr []int, sum int) {
+	n := len(arr)
+	left := 0
+	right := n - 1
+
+	sum:=
+	for left != right {
+
+	}
+}
